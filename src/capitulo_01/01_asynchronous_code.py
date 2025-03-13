3185776efc4c7c47f52f7d1d6547ff2ea210092f
Jose Alonzo <pepemxl@gmail.com>
2025-03-13 09:39:32 -0600

    Adding id for task

--- src/capitulo_01/01_asynchronous_code.py
+++ src/capitulo_01/01_asynchronous_code.py
@@ -3,15 +3,15 @@ import time
 
 
 # async def: Define una función asíncrona.
-async def tarea():
-    print("Inicio de tarea")
+async def tarea(id: int):
+    print("Inicio de tarea", id)
     # await asyncio.sleep(2): Suspende la ejecución y permite que otras tareas corran mientras espera.
     await asyncio.sleep(2)  # Simula una operación asíncrona
-    print("Tarea completada")
+    print("Tarea completada", id)
 
 async def main():
     # asyncio.gather(): Ejecuta múltiples tareas en paralelo.
-    await asyncio.gather(tarea(), tarea())  # Ejecuta dos tareas en paralelo
+    await asyncio.gather(tarea(1), tarea(2))  # Ejecuta dos tareas en paralelo
 
 
 if __name__ == '__main__':
