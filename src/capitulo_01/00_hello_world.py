c40cda04a81508b5134531e475604ebd9df212f0
Jose Alonzo <pepemxl@gmail.com>
2025-03-16 09:20:42 -0600

    Adding basic example of asyncio

--- src/capitulo_01/00_hello_world.py
+++ src/capitulo_01/00_hello_world.py
@@ -3,20 +3,15 @@ import time
 
 
 async def say_after(delay, message):
-    # print("Inicio con delay", delay)
+    print(f"Start with delay of {delay} segs")
     await asyncio.sleep(delay)
     print(message, end=' ')
 
 async def main():
-    print(f"started at {time.strftime('%X')}")
-    # tasks = []
-    # tasks.append(await say_after(3, 'pepe'))
-    # tasks.append(await say_after(2, 'World'))
-    # tasks.append(await say_after(1, 'Hello'))
+    print(f"Started at {time.strftime('%X')}")
     await asyncio.gather(say_after(3, 'pepe'),say_after(2, 'World'), say_after(1, 'Hello'))
-    #await asyncio.gather_all(tasks)
     print('')
-    print(f"finished at {time.strftime('%X')}")
+    print(f"Finished at {time.strftime('%X')}")
 
 
 if __name__ == '__main__':
