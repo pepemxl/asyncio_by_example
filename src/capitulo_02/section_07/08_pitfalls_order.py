3939292902d8fc588add771bd4a104a0e43d475e
Jose Alonzo <pepemxl@gmail.com>
2025-08-26 06:50:01 -0600

    updates

--- src/capitulo_02/section_07/08_pitfalls_order.py
+++ src/capitulo_02/section_07/08_pitfalls_order.py
@@ -7,23 +7,34 @@ from util.async_timer import async_timed
 from util.delay_functions import delay
 from util.sync_timed import sync_timed
 
+counter = 0
+
+async def add_counter():
+    global counter
+    counter = counter + 1
+    return counter
+
 @async_timed()
 async def cpu_bound_work(task_id) -> int:
+    global counter
     counter = 0
     for i in range(100000000):
-        counter = counter + 1
+        # counter = counter + 1
+        await add_counter()
     return counter
 
 
 async def test_01() -> None:
     print("---Empezando test 01---")
     task_one = asyncio.create_task(cpu_bound_work(task_id=1))
+    task_two = asyncio.create_task(cpu_bound_work(task_id=2))
     await task_one
+    await task_two
     print("-----------------------")
 
 async def test_02() -> None:
-    print("Empezando test 02")
-    task_delay = asyncio.create_task(delay(5,1))
+    print("---Empezando test 01---")
+    task_delay = asyncio.create_task(delay(2,1))
     task_one = asyncio.create_task(cpu_bound_work(task_id=2))
     task_two = asyncio.create_task(cpu_bound_work(task_id=3))
     await task_one
@@ -32,19 +43,20 @@ async def test_02() -> None:
     print("-----------------------")
 
 async def test_03() -> None:
-    print("Empezando test 03")
-    task_one = asyncio.create_task(cpu_bound_work(task_id=1))
-    task_two = asyncio.create_task(cpu_bound_work(task_id=2))
-    task_delay = asyncio.create_task(delay(5,3))
+    print("---Empezando test 01---")
+    task_one = asyncio.create_task(cpu_bound_work(task_id=2))
+    task_two = asyncio.create_task(cpu_bound_work(task_id=3))
+    task_delay = asyncio.create_task(delay(2,1))
     await task_one
     await task_two
     await task_delay
     print("-----------------------")
 
+
 @sync_timed()
 def main() -> None:
-    asyncio.run(test_01())
-    asyncio.run(test_02())
+    #asyncio.run(test_01())
+    #asyncio.run(test_02())
     asyncio.run(test_03())
 
 
